b2969f870966556ce86e14dde6a4ec5f53e91836
Christophe Irles <christophe.irles@al-enterprise.com>
2026-08-19 10:50:36 +0200

    ScopedLogger: allow to create ILogger with a min log level LogFactory: aloow to set a min log level to all ILogger based on their prefix

--- LogFactory.cs
+++ LogFactory.cs
@@ -80,9 +80,51 @@ namespace Sharp.Xmpp
             Instance._factory = factory;
         }
 
+        /// <summary>
+        /// To set the <see cref="IScopedLoggers"/> used for logging purpose.
+        /// 
+        /// This method must be called before to use the SDK.
+        /// 
+        /// **Note:** By default use **Sharp.Xmpp.ScopedLoggers** object
+        /// </summary>
+        /// <param name="scopedLoggers"><see cref="IScopedLoggers"/> interface</param>
         public static void SetScopedLoggers(IScopedLoggers scopedLoggers)
         {
             Instance._scopedLoggers = scopedLoggers;
+        }
+
+        /// <summary>
+        /// To change, at runtime, the minimum <see cref="LogLevel"/> for a given logger instance
+        /// (identified by its prefix, as passed to <see cref="CreateLogger(string, string)"/>).
+        /// 
+        /// Has no effect if no <see cref="IScopedLoggers"/> has been set (see <see cref="SetScopedLoggers"/>),
+        /// or if <paramref name="prefix"/> is null or empty.
+        /// </summary>
+        /// <param name="prefix"><see cref="string"/> Prefix identifying the logger instance</param>
+        /// <param name="logLevel"><see cref="LogLevel"/> New minimum level for this instance</param>
+        public static void SetLevel(string prefix, LogLevel logLevel)
+        {
+            if (string.IsNullOrEmpty(prefix))
+                return;
+
+            Instance._scopedLoggers?.SetLevel(prefix, logLevel);
+        }
+
+        /// <summary>
+        /// To get the current minimum <see cref="LogLevel"/> configured for a given logger instance
+        /// (identified by its prefix).
+        /// 
+        /// Returns <see cref="LogLevel.Debug"/> as default if no <see cref="IScopedLoggers"/> has been set,
+        /// or if <paramref name="prefix"/> is null or empty, or if no level was ever set for this prefix.
+        /// </summary>
+        /// <param name="prefix"><see cref="string"/> Prefix identifying the logger instance</param>
+        /// <returns><see cref="LogLevel"/> - Current minimum level for this instance</returns>
+        public static LogLevel GetLevel(string prefix)
+        {
+            if (string.IsNullOrEmpty(prefix) || Instance._scopedLoggers is null)
+                return LogLevel.Debug;
+
+            return Instance._scopedLoggers.GetLevel(prefix);
         }
 
         private LogFactory()
--- ScopedLogger.cs
+++ ScopedLogger.cs
@@ -1,40 +1,119 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Sharp.Xmpp
 {
+    /// <summary>
+    /// <see cref="IScopedLoggers"/> interface definition.
+    /// Used by <see cref="LogFactory"/> to create <see cref="ILogger"/>
+    /// </summary>
     public interface IScopedLoggers
     {
-        public ILogger Create(ILogger inner, string loggerPrefix);
+        /// <summary>
+        /// To create a <see cref="ILogger"/> based on the specified one.
+        /// </summary>
+        /// <param name="inner"><see cref="ILogger"/>ILogger object</param>
+        /// <param name="loggerPrefix"><see cref="String"/>Prefix to use</param>
+        /// <param name="logLevel"><see cref="LogLevel"/>Minimum Log level to use</param>
+        /// <returns><see cref="ILogger"/> - ILogger object</returns>
+        ILogger Create(ILogger inner, string loggerPrefix, LogLevel logLevel = LogLevel.Debug);
+
+        /// <summary>
+        /// Change Log Level for all loggers with the specified prefix.
+        /// </summary>
+        /// <param name="loggerPrefix">The prefix for the loggers to modify.</param>
+        /// <param name="level">The new log level.</param>
+        void SetLevel(string loggerPrefix, LogLevel level);
+
+        /// <summary>
+        /// Get current log level for the logger with the specified prefix.
+        /// </summary>
+        /// <param name="loggerPrefix">The prefix for the logger to query.</param>
+        /// <returns>The current log level.</returns>
+        LogLevel GetLevel(string loggerPrefix);
     }
 
+    /// <summary>
+    /// Based on <see cref="IScopedLoggers"/> interface.
+    /// Permit to create <see cref="ScopedLogger"/> and change easily Log Level of all of them based on their prefix.
+    /// </summary>
     public class ScopedLoggers : IScopedLoggers
     {
-        public ILogger Create(ILogger inner, string loggerPrefix)
+        // Thread-safe
+        private readonly ConcurrentDictionary<string, LogLevel> _levels = new();
+
+        /// <summary>
+        /// To create a <see cref="ScopedLogger"/> (inherits from <see cref="ILogger"/>) based :
+        /// - on the specified <see cref="ILogger"/> 
+        /// - and on the specified prefix
+        /// </summary>
+        /// <param name="inner"><see cref="ILogger"/>ILogger object</param>
+        /// <param name="loggerPrefix"><see cref="String"/>Prefix to use</param>
+        /// <param name="logLevel"><see cref="LogLevel"/>Minimum Log level to use</param>
+        /// <returns><see cref="ILogger"/> - <see cref="ScopedLogger"/> object</returns>
+        public ILogger Create(ILogger inner, string loggerPrefix, LogLevel logLevel = LogLevel.Debug)
         {
-            return new ScopedLogger(inner, loggerPrefix);
+            _levels.AddOrUpdate(loggerPrefix, logLevel, (_, _) => logLevel);
+            return new ScopedLogger(inner, loggerPrefix, this);
         }
+
+        /// <summary>
+        /// Change log level for all loggers with the specified prefix.
+        /// </summary>
+        /// <param name="loggerPrefix">The prefix for the loggers to modify.</param>
+        /// <param name="level">The new log level.</param>
+        public void SetLevel(string loggerPrefix, LogLevel level)
+        {
+            _levels[loggerPrefix] = level;
+        }
+
+        /// <summary>
+        /// Get current log level for the logger with the specified prefix.
+        /// </summary>
+        /// <param name="loggerPrefix">The prefix for the logger to query.</param>
+        /// <returns>The current log level.</returns>
+        public LogLevel GetLevel(string loggerPrefix) =>
+            _levels.TryGetValue(loggerPrefix, out var level) ? level : LogLevel.Debug;
     }
 
     public class ScopedLogger : ILogger
     {
         private readonly ILogger _inner;
+        private readonly string _loggerPrefix;
+        private readonly IScopedLoggers _levels;
         private readonly Dictionary<string, object> _state;
 
-        public ScopedLogger(ILogger inner, string loggerPrefix)
+        public ScopedLogger(ILogger inner, string loggerPrefix, IScopedLoggers levels)
         {
             _inner = inner;
+            _loggerPrefix = loggerPrefix;
+            _levels = levels;
             _state = new Dictionary<string, object> { ["LoggerPrefix"] = loggerPrefix };
         }
 
         public IDisposable BeginScope<TState>(TState state) => _inner.BeginScope(state);
 
-        public bool IsEnabled(LogLevel logLevel) => _inner.IsEnabled(logLevel);
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            // The level specific to this instance must be respected.
+            var instanceLevel = _levels.GetLevel(_loggerPrefix);
+            if (logLevel < instanceLevel)
+                return false;
+
+            // The settings of the underlying ILogger (e.g., global minimum level) are also respected.
+            return _inner.IsEnabled(logLevel);
+        }
 
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state,
             Exception exception, Func<TState, Exception, string> formatter)
         {
+            // For security, if Log() is called immediatly without using IsEnabled
+            if (!IsEnabled(logLevel))
+                return;
+
             using (_inner.BeginScope(_state))
             {
                 _inner.Log(logLevel, eventId, state, exception, formatter);
